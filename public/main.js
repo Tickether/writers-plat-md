364c6ddc6d5d011ef5dc8dc25c951d004ce3425d
Tickether <tickether@gmail.com>
2023-04-12 17:29:49 +0000

    init push

---
+++ public/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+
+require('@electron/remote/main').initialize()
+
+function createWindow() {
+    // create window
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            enableRemoteModule: true
+        }
+    })
+
+    win.loadURL('http://localhost:3000')
+}
+
+app.on('ready', createWindow)
+
+// quit when windows close
+app.on('window-all-closed', function () {
+    if(process.platform !== 'darwin'){
+        app.quit()
+    }
+})
+
+app.on('activate', function () {
+    if(BrowserWindow.getAllWindows().length !== 0){
+        createWindow()
+    }
+})
---
+++ src/Editor.js
@@ -0,0 +1,45 @@
+import "@bangle.dev/core/style.css";
+import "@bangle.dev/tooltip/style.css";
+import "@bangle.dev/react-menu/style.css";
+
+import { useEditorState, BangleEditor } from "@bangle.dev/react";
+import { PluginKey } from "@bangle.dev/core";
+import { floatingMenu, FloatingMenu } from "@bangle.dev/react-menu";
+import {
+  bold,
+  listItem,
+  bulletList,
+  orderedList,
+} from "@bangle.dev/base-components";
+
+const menuKey = new PluginKey("menuKey");
+
+export function Editor() {
+  const editorState = useEditorState({
+    initialValue: "Hello world!",
+    specs: [
+      bold.spec(),
+      bulletList.spec(),
+      orderedList.spec(),
+      listItem.spec(),
+    ],
+    plugins: () => [
+      bold.plugins(),
+      bulletList.plugins(),
+      orderedList.plugins(),
+      listItem.plugins(),
+      floatingMenu.plugins({
+        key: menuKey,
+      }),
+    ],
+  });
+
+  return (
+    <BangleEditor
+      state={editorState}
+      style={{ margin: "10px 20px 0 20px", backgroundColor: "white" }}
+    >
+      <FloatingMenu menuKey={menuKey} />
+    </BangleEditor>
+  );
+}
